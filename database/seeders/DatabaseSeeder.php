1c630f18b97843f44d8f7905ca20aea239f8f6b0
couchjanus <couchjanus@gmail.com>
2021-03-22 21:37:30 +0200

    added unit 6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(CategoriesTableSeeder::class);
+        // $this->call(CategoriesTableSeeder::class);
+        \App\Models\Brand::factory(20)->create();
     }
 }
